17eb6925210683b458f568e62830b35afd5dae8f
Lori J <mintblue87@gmail.com>
2012-11-05 11:19:16 -0800

    Default to asset pipeline in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ PhotographyWebsite::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+#just changed this to true, seems to make it work in production
 
   # Generate digests for assets URLs
   config.assets.digest = true
