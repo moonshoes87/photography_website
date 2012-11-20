aead5844b0ca11385e83c66fd5d2511f53d76fcb
Lori J <mintblue87@gmail.com>
2012-11-20 13:20:37 -0800

    Layout for photos index page

--- app/assets/javascripts/full_page.js
+++ app/assets/javascripts/full_page.js
@@ -1,21 +1,13 @@
-// you will need to make the background stuff not be "I am the header" and all
-
-
 $(document).ready(function(){
-	// MAKE IT SO IT HIDES THE NAV BAR!!!!!!
-	$(".holder").hide();
+	//UNCOMMENT OUT THE TEXT BELOW, ONCE YOU GET THE JAVASCRIPTS ALL SORTED
+	//$(".holder").hide();
 	//hides all the other images
 	$("div#bg > img:gt(0)").hide();
-	// make navbar invisible  
 	$("div#bg img").click(function(){
 		start_slideshow();
 		enter_navbar();
-		//use_navbar();
 		$('div#bg img').unbind('click');
 	    });
-
-	    // make it so that when the img is clicked on, it instantiates two functions: one that starts a slideshow
-	    // the other one that brings in the nav bar.  then, it needs to be unbound to prevent that happening again
     });
 
 
@@ -36,30 +28,3 @@ function enter_navbar(){
 	    return true;
 	});
 }
-    
-/*
-function use_navbar(){
-    var $photographs = $(".holder .navigation .nav li:eq(2)");
-    $photographs.click(function(e){
-	    e.preventDefault();
-	    $(".dropdown").toggle();
-	});
-    $photographs.hover(function(){
-	    $(".dropdown").show();
-	},
-	function(){
-	    $(".dropdown").hide();
-	    //    $(".dropdown").delay().fadeOut();
-	});
-    $(".dropdown").hover(function(){
-	    $(this).show();
-	},
-	function(){ 
-	    $(this).hide();
-	});
-}
-	       
-       
-*/
-    //   $(".holder .navigation .nav li:eq(2)").click( show the drop down menu )
-    //make it show, and slideUp()
