1bb5069c607e03320ee1fe6753dfc2ce4d516438
Lori J <mintblue87@gmail.com>
2012-11-30 16:21:31 -0800

    add to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,6 @@ PhotographyWebsite::Application.configure do
 
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  
+  config.action_mailer.raise_delivery_errors = true  
+
 end
