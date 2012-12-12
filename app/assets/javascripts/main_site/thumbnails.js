1fffd8a4ddfa930ce08067dcb6af629899c783f9
Lori J <mintblue87@gmail.com>
2012-12-12 13:46:44 -0800

    add scrolling to thumbnails

--- app/assets/javascripts/main_site/thumbnails.js
+++ app/assets/javascripts/main_site/thumbnails.js
@@ -1,9 +1,15 @@
+// maybe cycle through images by appending them, like with the css code snippets slideshow 
+
 $(document).ready(function(){
 	$("li.nail").click(function(){
 		var html = $(this).html();
 		$("#display").html(html).addClass("image");
 	    });
-	$("#click-back, #click-forward").click(function(){
-		alert("you just clicked");
+	$("#click-forward").on("click", function(){
+		$("ul#thumbnails").append($("li.nail:lt(4)"));
+	    });
+	$("#click-back").click(function(){
+		var last_four = $("li.nail").length - 4;
+		    $("ul#thumbnails").prepend($("li.nail:gt(" + last_four + ")"));
 	    });
     });
