10b280fc977329370a7c58ec5bd75a403d7bd9a1
Lori J <mintblue87@gmail.com>
2012-12-29 18:40:28 -0800

    thumbnails centerer now working, but the calculations are somehow incorrect -- must fix margin

--- app/assets/javascripts/main_site/thumbnails.js
+++ app/assets/javascripts/main_site/thumbnails.js
@@ -3,6 +3,7 @@
 var photos_shown = [];
 
 $(document).ready(function(){
+        landscapes_vs_portraits();
 	$("li.nail:gt(4)").hide();
 	$("li.nail").click(function(){
 		var html = $(this).html();
@@ -12,6 +13,7 @@ $(document).ready(function(){
 		$("ul#thumbnails").append($("li.nail:lt(5)"));
 		$("li.nail:lt(5)").show();
 		$("li.nail:gt(5)").hide();
+	        landscapes_vs_portraits();
 		// ADD THIS NEXT.  Maybe use the slide function to 
 	    });
 	$("#click-back").click(function(){
@@ -20,13 +22,13 @@ $(document).ready(function(){
 	    });
 
 	function landscapes_vs_portraits(){
+	    console.log("landscapes_vs_portraits was called");
 	    //make this get called so that the landscape and portrait variables get reset each time the clicking happens
-	}
-	var landscapes = 0;
-	var portraits = 0;
-	var photo_array = $("li.nail img");
-        photos_shown = photo_array.slice(0,5);
-	function figure(){
+	    var landscapes = 0;
+	    var portraits = 0;
+	    var photo_array = $("li.nail img");
+            photos_shown = photo_array.slice(0,5);
+	    function figure(){
 	    for (i=0; i < 5; i++){
 		if ($(photos_shown[i]).hasClass("portrait")){
 		    portraits += 1;
@@ -40,17 +42,20 @@ $(document).ready(function(){
 		}
 	    }
 	}
-	figure();
-	console.log("Portraits:" + portraits + " and Landscapes: " + landscapes);
-var landscapes_width = landscapes * 100;
-var portraits_width = portraits * 80;
-var total_padding = 18;
-var how_many_photos = landscapes + portraits;
-var total_width = landscapes_width + portraits_width + total_padding;
-var necessary_margin = (600 - total_width) / 2;
-console.log("total width: " + total_width);
-console.log("necessary margin: " + necessary_margin)
-
+	    figure();
+	    console.log("Portraits:" + portraits + " and Landscapes: " + landscapes);
+	    var landscapes_width = landscapes * 100;
+	    var portraits_width = portraits * 80;
+	    var total_padding = 18;
+	    var how_many_photos = landscapes + portraits;
+	    var total_width = landscapes_width + portraits_width + total_padding;
+	    var necessary_margin = (600 - total_width) / 2;
+	    console.log("total width: " + total_width);
+	    console.log("necessary margin: " + necessary_margin)
+	    $("#thumbnails li:first-child").css("margin-left", necessary_margin + "px");
+            landscapes = 0;
+	    portraits = 0;
+	};
     });
 /*
 
